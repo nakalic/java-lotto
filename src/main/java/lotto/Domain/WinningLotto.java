8fcb205c89a94467f2d3f5dc2e8d9179a601c08d
nakalic <nakalic28@gmail.com>
2022-12-10 01:07:35 +0900

    refactor(WinningLotto): LottoNumber 생성자 변경에 따른 리팩토링

--- src/main/java/lotto/Domain/WinningLotto.java
+++ src/main/java/lotto/Domain/WinningLotto.java
@@ -6,10 +6,11 @@ public class WinningLotto {
     private Lotto winningNumbers;
     private LottoNumber bonusNumber;
 
+    //매개변수로 Lotto, LottoNumber 받고 싶은데 getter 없이 어떻게 ?
     public WinningLotto(List<Integer> numbers, int number) {
         validateBonusNumber(numbers, number);
         this.winningNumbers = new Lotto(numbers);
-        this.bonusNumber = new LottoNumber(number);
+        this.bonusNumber = new LottoNumber(Integer.toString(number));
     }
 
     private void validateBonusNumber(List<Integer> numbers, int number) {
