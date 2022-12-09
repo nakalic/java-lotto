48191aa03cfd9fc7ddf018f0b1cc3304ac3e14c5
nakalic <nakalic28@gmail.com>
2022-12-10 00:36:57 +0900

    feat(LottoMaker): 투입금액에 따른 로또 발행하는 기능 구현

--- src/main/java/lotto/Service/LottoMaker.java
+++ src/main/java/lotto/Service/LottoMaker.java
@@ -1,9 +1,21 @@
 package lotto.Service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import lotto.Domain.Lotto;
 
 public class LottoMaker {
+
+    public List<Lotto> makeManyLotto(int amount) {
+        List<Lotto> manyLotto = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            manyLotto.add(new Lotto(makeLotto()));
+        }
+        return manyLotto;
+    }
+
     public List<Integer> makeLotto() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
