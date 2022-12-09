7feb09a32229c31a837338de2114eeee8534bf3c
nakalic <nakalic28@gmail.com>
2022-12-10 01:02:50 +0900

    fix(LottoNumber): 로또 번호를 잘못 검증하던 버그 수정

--- src/main/java/lotto/Domain/LottoNumber.java
+++ src/main/java/lotto/Domain/LottoNumber.java
@@ -3,7 +3,27 @@ package lotto.Domain;
 public class LottoNumber {
     private int number;
 
-    public LottoNumber(int num) {
-        this.number = num;
+    public LottoNumber(String input) {
+        validateLottoNumber(input);
+        this.number = Integer.parseInt(input);
+    }
+
+    private void validateLottoNumber(String input) {
+        isNumber(input);
+        checkNumberRange(input);
+    }
+
+    private void isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야 합니다.");
+        }
+    }
+
+    private void checkNumberRange(String input) {
+        if (Integer.parseInt(input) < 1 || Integer.parseInt(input) > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 }
