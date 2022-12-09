5edcf9a51389d5ec709ffdbe410b56a3ad2eaabd
nakalic <nakalic28@gmail.com>
2022-12-09 18:40:00 +0900

    feat(Lotto): 로또 번호 검증 및 예외처리 기능 구현

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -1,20 +1,40 @@
 package lotto.Domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLotto(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateLotto(List<Integer> numbers) {
+        checkCounts(numbers);
+        checkNumberRange(numbers);
+        checkEachMustBeDifferent(numbers);
+    }
+
+    private void checkCounts(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 총 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void checkNumberRange(List<Integer> numbers) {
+        numbers.stream()
+                .filter(num -> num < 1 || num > 45)
+                .findAny()
+                .ifPresent(number -> {
+                    throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+                });
+    }
+
+    private void checkEachMustBeDifferent(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 모두 달라야합니다.");
+        }
+    }
 }
