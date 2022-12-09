42f0212226f9e953bdd24a5d556563cdb907ca67
nakalic <nakalic28@gmail.com>
2022-12-10 00:37:57 +0900

    feat(OutputView): 구입한 개수와, 구입한 로또 번호를 출력해주는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
+import lotto.Domain.LottoTickets;
+import lotto.Domain.Money;
+
 public class OutputView {
 
     public void printStart() {
-        System.out.println("구입금액을 입력 주세요.\n");
+        System.out.println("구입금액을 입력 주세요.");
     }
 
+    public void printAmount(Money money) {
+        System.out.println(money + "개를 구매했습니다\n");
+    }
+
+    public void printPublishedLottoTickets(LottoTickets lottoTickets) {
+        System.out.println(lottoTickets);
+    }
 }
