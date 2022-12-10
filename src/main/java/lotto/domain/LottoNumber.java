a93b67853ea40d12e4edc89160213dfa9825b9ac
nakalic <nakalic28@gmail.com>
2022-12-10 19:56:13 +0900

    feat: 로또 번호를 관리하는 클래스 생성

---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+public class LottoNumber {
+    private int number;
+
+    public LottoNumber(String input) {
+        validateLottoNumber(input);
+        this.number = Integer.parseInt(input);
+    }
+
+    private void validateLottoNumber(String input) {
+        checkIsNumber(input);
+        checkNumberRange(input);
+    }
+
+    private void checkIsNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 숫자여야 합니다.");
+        }
+    }
+
+    private void checkNumberRange(String input) {
+        if (Integer.parseInt(input) < 1 || Integer.parseInt(input) > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumber{" +
+                "number=" + number +
+                '}';
+    }
+}
