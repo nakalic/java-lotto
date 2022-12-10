e4d86ed562057bb4ea94b39e6ba7b5c22e573b95
nakalic <nakalic28@gmail.com>
2022-12-11 00:18:18 +0900

    feat: eof 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,10 +4,9 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
+        // 예외 처리 어디서 할 지?
+        // 오름차순 정렬 stream -> ClassCastException comparable 해결
         LottoController lottoController = new LottoController();
         lottoController.run();
     }
 }
-
-// 예외 처리 어디서 할 지?
-// 오름차순 정렬 stream -> ClassCastException comparable 해결
