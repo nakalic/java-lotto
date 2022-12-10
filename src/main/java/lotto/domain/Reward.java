0d4908b53e07eb818b13cff4e8f9611a3a3d89bd
nakalic <nakalic28@gmail.com>
2022-12-10 20:46:14 +0900

    feat: 당첨 금액에 관한 클래스 생성

---
+++ src/main/java/lotto/domain/Reward.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+public enum Reward {
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000);
+
+    private int reward;
+
+    Reward(int reward) {
+        this.reward = reward;
+    }
+
+    @Override
+    public String toString() {
+        return "Reward{" +
+                "reward=" + reward +
+                '}';
+    }
+}
