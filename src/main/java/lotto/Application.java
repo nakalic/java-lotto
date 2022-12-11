afafe6cbb312584ff1a02b6fc889701f867d4e6f
nakalic <nakalic28@gmail.com>
2022-12-11 14:26:49 +0900

    feat: 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,9 +4,11 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        // 예외 처리 어디서 할 지?
-        // 오름차순 정렬 stream -> ClassCastException comparable 해결
-        LottoController lottoController = new LottoController();
-        lottoController.run();
+        try {
+            LottoController lottoController = new LottoController();
+            lottoController.run();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
